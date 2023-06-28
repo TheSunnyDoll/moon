3eeaaaa306a05186a2c7a6b800fa282ae0cbaea0
KIMQI001 <297511521@qq.com>
2023-06-28 17:04:17 +0800

    follower

--- core/follower.py
+++ core/follower.py
@@ -51,6 +51,41 @@ class Follower(Bitget):
             print(f"place order : {exo}")
             # print(order)
 
+    def check_long_position(self,ld_positions,fl_positions):
+        pct_long_qty = round(float(ld_positions['long']['qty']) * pct , 3)
+        fl_long_qty = round(float(fl_positions['long']['qty']) , 3)
+
+        if pct_long_qty == fl_long_qty:
+            return 0
+        else:
+            return abs(pct_long_qty - fl_long_qty)
+        
+
+    def check_short_position(self,ld_positions,fl_positions):
+        pct_short_qty = round(float(ld_positions['short']['qty']) * pct , 3)
+        fl_short_qty = round(float(fl_positions['short']['qty']) , 3)
+
+        if pct_short_qty == fl_short_qty:
+            return 0
+        else:
+            return abs(pct_short_qty - fl_short_qty)
+        
+    def cut_qty(self,diff_long,diff_short):
+        if diff_long != 0:
+            print(diff_long)
+            # self.create_order(symbol,'market','close_long',diff_long,reduce_only=True)
+        elif diff_short != 0:
+            print(diff_short)
+            # self.create_order(symbol,'market','close_short',diff_short,reduce_only=True)
+
+    def cancel_orders(self,symbol,cancel_orders):
+        for order in cancel_orders:
+            if order[2] == 'open_short':
+                print(f"cancle: {order}")
+                #self.cancel_short_entry_bitget(symbol)
+            elif order[2] == 'open_long':
+                print(f"cancle: {order}")
+                #self.cancel_long_entry_bitget(symbol)
 
 if __name__ == '__main__':
     symbol = 'ETHUSDT_UMCBL'
@@ -67,13 +102,14 @@ if __name__ == '__main__':
         pct = round(float(fl_dex)/float(ld_dex) , 2)
         print(pct)
     #     # check positions
-    #     ## if same , continue
-        positions = ld.get_positions_bitget(symbol)
-        print(positions)
-    #     ## if not same , clear 
+        ld_positions = ld.get_positions_bitget(symbol)
+        fl_positions = fl.get_positions_bitget(symbol)
+
+        diff_long = fl.check_long_position(ld_positions,fl_positions)
+        diff_short = fl.check_short_position(ld_positions,fl_positions)
 
     #     ## if partial not same , cut the size
-
+        fl.cut_qty(diff_long,diff_short)
 
 
         # get ld orders
@@ -89,10 +125,10 @@ if __name__ == '__main__':
         cancel_orders,exec_orders = fl.check_order_exist(pct_orders,fl_orders)
 
         ## cancel orders
-
+        fl.cancel_orders(symbol,cancel_orders)
+        
         ## exec orders
-
-        print(fl_orders)
+        print(exec_orders)
         ## copy trader
         if exec_orders != []:
             fl.copy_trade(symbol,exec_orders)
