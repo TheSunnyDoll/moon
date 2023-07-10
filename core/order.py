c9cb5263a72a955a2d295a501fca4a3b73b5f1db
KIMQI001 <297511521@qq.com>
2023-07-10 17:15:57 +0800

    order by hand

---
+++ core/order.py
@@ -0,0 +1,27 @@
+from pybitget import Client
+import argparse
+from utils import *
+
+
+symbol = 'BTCUSDT_UMCBL'
+marginCoin = 'USDT'
+qty = 0.5
+side = 'open_short'
+entry = 30620
+tp = 29700
+sl = 31000
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--username', help='Username')
+parser.add_argument('-d', '--debug_mode', action='store_true', default=False, help='Enable debug mode')
+
+args = parser.parse_args()
+heroname = args.username
+debug_mode = args.debug_mode
+
+config = get_config_file()
+hero = config[heroname]
+
+huFu = Client(hero.api_key,hero.secret_key, hero.passphrase)
+huFu.mix_place_plan_order(symbol, marginCoin, qty , side, 'limit', entry, "market_price", executePrice=entry,presetTakeProfitPrice=tp, presetStopLossPrice=sl, reduceOnly=False)
