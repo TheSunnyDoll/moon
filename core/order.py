d8ba235e33c5c8b2265d9b8114c6eea05354187d
KIMQI001 <297511521@qq.com>
2023-07-11 11:15:21 +0800

    order by hand

--- core/order.py
+++ core/order.py
@@ -26,4 +26,36 @@ hero = config[heroname]
 huFu = Client(hero['api_key'],hero['secret_key'],hero['passphrase'])
 huFu.mix_place_plan_order(symbol, marginCoin, qty , side, 'limit', entry, "market_price", executePrice=entry,presetTakeProfitPrice=tp, presetStopLossPrice=sl, reduceOnly=False)
 data = huFu.mix_get_plan_order_tpsl(symbol=symbol,isPlan='plan')['data']
-print(data)
+for data in data:
+    print(data)
+
+
+
+new_long_sl = 0
+new_short_sl = 29801
+data = huFu.mix_get_plan_order_tpsl(symbol=symbol,isPlan='profit_loss')['data']
+for plan in data:
+    if plan['planType'] == 'loss_plan':
+        if plan['side'] == 'close_long' and new_long_sl != 0:
+            ## modifiy the sl
+            try:
+                size = plan['size']
+                huFu.mix_cancel_plan_order(symbol, marginCoin, plan['orderId'], 'loss_plan')
+                huFu.mix_place_stop_order(symbol, marginCoin, new_long_sl, 'loss_plan', 'long',triggerType='fill_price', size=size, rangeRate=None)      
+                print(f"士气正盛！前移败退点！北军 新败退点: {new_long_sl} ")
+
+            except Exception as e:
+                print(f"move long sl faild, order id is {plan['orderId']},new_long_sl is {new_long_sl} ,{e}")
+            
+        elif plan['side'] == 'close_short' and new_short_sl != 0:
+            if new_short_sl < float(plan['triggerPrice']):
+                ## modifiy the sl
+                try:
+                    size = plan['size']
+                    huFu.mix_cancel_plan_order(symbol, marginCoin, plan['orderId'], 'loss_plan')
+                    huFu.mix_place_stop_order(symbol, marginCoin, new_short_sl, 'loss_plan', 'short',triggerType='fill_price', size=size, rangeRate=None)                            
+                    print(f"士气正盛！前移败退点！ 南军 新败退点:  {new_short_sl} ")
+
+                except Exception as e:
+                    print(f"move short sl faild, order id is {plan['orderId']},new_short_sl is {new_short_sl} ,{e}")
+
