104784b03a2a6db9f5cc2048b259b70c14644e1c
KIMQI001 <297511521@qq.com>
2023-07-18 12:05:35 +0800

    NY range

--- core/test1.py
+++ core/test1.py
@@ -1,51 +1,55 @@
-import requests
-import time
+from utils import *
+from pybitget import Client
+import argparse
 
-url = "https://api.bitget.com/api/mix/v1/market/candles"
-symbol = "BTCUSDT_UMCBL"
-granularity = '1m'
+# Function to find pivot highs and pivot lows
+def find_pivot_highs_lows(data, lookback):
+    pivot_highs = []
+    pivot_lows = []
 
-# 获取当前时间戳和5小时前的时间戳
-current_time = int(time.time())
-start_time = current_time - (5 * 60 * 60)
+    for i in range(lookback, len(data) - lookback):
+        if all(data[i] > data[i - lookback:i]) and all(data[i] >= data[i + 1:i + lookback + 1]):
+            pivot_highs.append((i, data[i]))
+        elif all(data[i] < data[i - lookback:i]) and all(data[i] <= data[i + 1:i + lookback + 1]):
+            pivot_lows.append((i, data[i]))
 
-params = {
-    "symbol": symbol,
-    "granularity": granularity,
-    "startTime": start_time,
-    "endTime": current_time
-}
+    return pivot_highs, pivot_lows
 
-response = requests.get(url, params=params)
-data = response.json()
+# Parse the command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--username', help='Username')
 
-if isinstance(data, list):
-    candles = data
+args = parser.parse_args()
+heroname = args.username
 
-    swing_highs = []
-    swing_lows = []
+# Get API credentials from the configuration file
+config = get_config_file()
+hero = config[heroname]
+symbol = 'BTCUSDT_UMCBL'
+huFu = Client(hero['api_key'], hero['secret_key'], hero['passphrase'])
 
-    for i in range(2, len(candles)):
-        current_high = float(candles[i][2])
-        prev_high = float(candles[i - 1][2])
-        next_high = float(candles[i + 1][2])
+# Get the timestamps for the desired data range
+startTime = get_previous_three_hour_timestamp()
+endTime = get_previous_minute_timestamp()
 
-        current_low = float(candles[i][3])
-        prev_low = float(candles[i - 1][3])
-        next_low = float(candles[i + 1][3])
+# Retrieve BTC 3-hour 15-minute candlestick data
+data = huFu.mix_get_candles(symbol, '15m', startTime, endTime)
 
-        if current_high > prev_high and current_high > next_high:
-            swing_highs.append((candles[i][0], current_high))
-        elif current_low < prev_low and current_low < next_low:
-            swing_lows.append((candles[i][0], current_low))
+print(data['data'])
+# Extract the 'close' prices from the data
+close_prices = [float(item['close']) for item in data['data']]
 
-    print("Swing Highs:")
-    for swing_high in swing_highs:
-        print(f"Timestamp: {swing_high[0]}, Price: {swing_high[1]}")
+# Set the lookback value for finding pivot highs and pivot lows
+lookback = 5
 
-    print("\nSwing Lows:")
-    for swing_low in swing_lows:
-        print(f"Timestamp: {swing_low[0]}, Price: {swing_low[1]}")
-else:
-    error_message = data["msg"]
-    print(f"API请求错误：{error_message}")
+# Find pivot highs and pivot lows in the close prices data
+pivot_highs, pivot_lows = find_pivot_highs_lows(close_prices, lookback)
+
+# Print pivot highs and pivot lows
+print("Pivot Highs:")
+for high in pivot_highs:
+    print(f"Index: {high[0]}, Value: {high[1]}")
+
+print("\nPivot Lows:")
+for low in pivot_lows:
+    print(f"Index: {low[0]}, Value: {low[1]}")
