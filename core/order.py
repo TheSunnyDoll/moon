0f6161a025ae27847e80f53d6a19e468c019259c
KIMQI001 <297511521@qq.com>
2023-07-20 18:31:30 +0800

    hand

--- core/order.py
+++ core/order.py
@@ -65,9 +65,9 @@ if order :
    # side = 'open_long'
     side = 'open_short'
 
-    entry = 30980
+    entry = 30990
     tp = 29000
-    sl = 31080
+    sl = 31090
 
     huFu.mix_place_plan_order(symbol, marginCoin, qty , side, 'limit', entry, "market_price", executePrice=entry,presetTakeProfitPrice=tp, presetStopLossPrice=sl, reduceOnly=False)
 
