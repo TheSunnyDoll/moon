df5a0163ad3035a815dbd416ab2fdfc2a51a5409
KIMQI001 <297511521@qq.com>
2023-07-25 00:05:00 +0800

    commentary

--- core/utils.py
+++ core/utils.py
@@ -145,6 +145,43 @@ def get_logger(logfile='app.log'):
 def is_approximately_equal(value1, value2, tolerance=10):
     return abs(value1 - value2) <= tolerance
 
+def is_more_than_4hours(timestamps):
+# 给定的时间戳
+    timestamp = float(timestamps) / 1000  # 将毫秒转换为秒
+
+    # 获取当前时间戳
+    current_timestamp = time.time()
+
+    # 将时间戳转换为datetime对象
+    given_time = datetime.datetime.fromtimestamp(timestamp)
+    current_time = datetime.datetime.fromtimestamp(current_timestamp)
+    # 计算时间差
+    time_difference = current_time - given_time
+    # 判断时间差是否大于等于8小时
+    if time_difference.total_seconds() >= 4 * 3600:
+        return True
+    else:
+        return False
+
+def is_more_than_6hours(timestamps):
+# 给定的时间戳
+    timestamp = float(timestamps) / 1000  # 将毫秒转换为秒
+
+    # 获取当前时间戳
+    current_timestamp = time.time()
+
+    # 将时间戳转换为datetime对象
+    given_time = datetime.datetime.fromtimestamp(timestamp)
+    current_time = datetime.datetime.fromtimestamp(current_timestamp)
+    # 计算时间差
+    time_difference = current_time - given_time
+    # 判断时间差是否大于等于8小时
+    if time_difference.total_seconds() >= 6 * 3600:
+        return True
+    else:
+        return False
+
+
 def is_more_than_8hours(timestamps):
 # 给定的时间戳
     timestamp = float(timestamps) / 1000  # 将毫秒转换为秒
@@ -256,6 +293,24 @@ def remaining_time_to_8_hours(stop_loss_time):
 
     # 计算距离8小时还有多久
     remaining_time = datetime.timedelta(hours=8) - time_difference
+
+    # 将remaining_time转换为字符串格式
+    remaining_time_str = str(remaining_time)
+
+    return remaining_time_str
+
+def remaining_time_to_4_hours(stop_loss_time):
+    # 将stop_loss_time转换为datetime对象
+    stop_loss_time = datetime.datetime.fromtimestamp(int(stop_loss_time) / 1000)
+
+    # 获取当前时间
+    current_time = datetime.datetime.now()
+
+    # 计算时间差
+    time_difference = current_time - stop_loss_time
+
+    # 计算距离4小时还有多久
+    remaining_time = datetime.timedelta(hours=4) - time_difference
 
     # 将remaining_time转换为字符串格式
     remaining_time_str = str(remaining_time)
