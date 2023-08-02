4ca400e78adadd647d61c08d6260784686a2dc7e
KIMQI001 <297511521@qq.com>
2023-08-02 09:29:51 +0800

    handmode

--- core/order.py
+++ core/order.py
@@ -101,7 +101,7 @@ if close:
         data = huFu.mix_place_order(symbol,'USDT',short_qty,'close_short','market',reduceOnly=True)
 
 
-new_short_sl = 30010
+new_short_sl = 30110
 new_short_tp = 0
 
 new_long_sl = 0
