1bda1212803869d9f25e04e6ecb7ebd1f6bd567a
KIMQI001 <297511521@qq.com>
2023-08-03 16:07:33 +0800

    test mode

--- core/baseball.py
+++ core/baseball.py
@@ -878,7 +878,7 @@ def start(hero,symbol,marginCoin,debug_mode,fix_mode,fix_tp,base_qty,base_sl,max
 
         dtrend = bb.determine_trend(one_H_legs)
 
-        last_legs = bb.get_last_legs(dtrend,one_H_legs)
+        last_legs = bb.get_last_legs(dtrend,fif_legs)
         # one_H_legs = one_H_legs[1:]
         # legs = [[dtrend] + one_H_legs[1:] for dtrend, one_H_legs in zip(dtrend, one_H_legs)]
         # last_legs = [leg for leg in legs if leg[0] != 'bull_pullback' and leg[0] != 'bear_pullback']
