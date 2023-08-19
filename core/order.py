9cae3454e1f3f583f55090badc296f73b47b6ddf
KIMQI001 <297511521@qq.com>
2023-08-19 09:49:10 +0800

    get lever

--- core/order.py
+++ core/order.py
@@ -96,10 +96,6 @@ if order :
 if orderId != 0:
     huFu.mix_cancel_plan_order(symbol, marginCoin, orderId, 'normal_plan')
 
-print("leverage -------------------------")
-data = huFu.mix_get_leverage(symbol)
-print(data)
-
 print("plan orders -------------------------")
 data = huFu.mix_get_plan_order_tpsl(symbol=symbol,isPlan='plan')['data']
 for data in data:
